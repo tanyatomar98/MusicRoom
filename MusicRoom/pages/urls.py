95a5140b06c02f1c5bec9b8ce8a150daa43c9641
tanya <tanyatomar46@gmail.com>
2021-11-02 20:28:49 +0530

    Manipulating template

--- MusicRoom/pages/urls.py
+++ MusicRoom/pages/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home_page_view
+from .views import (
+    home_page_view,
+    sign_in_view
+                   )
 app_name = 'pages'
 
 urlpatterns = [
-    path('', home_page_view, name="pages")
+    path('', home_page_view, name="pages"),
+    path('signin/', sign_in_view, name="sign-in")
 ]
--- MusicRoom/pages/views.py
+++ MusicRoom/pages/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_page_view(request, *args, **kwargs):
     return render(request, 'base.html', {})
+
+def sign_in_view(request, *args, **kwargs):
+    return render(request, 'signin.html', {})
+
